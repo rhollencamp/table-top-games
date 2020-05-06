215615c3f7b601404363c17494c771b0ee18e008
Robert Hollencamp <rhollencamp@gmail.com>
2020-05-06 00:14:59 -0400

    messages on create/join

--- ttg/room.py
+++ ttg/room.py
@@ -12,16 +12,16 @@ _room_lock = Lock()
 def create_room(msg, wsock: WebSocket):
     with _room_lock:
         # generate a unique room code
-        room_code = _generate_room_code()
+        room_code = __generate_room_code()
         while room_code in _rooms:
-            room_code = _generate_room_code()
+            room_code = __generate_room_code()
 
         room = Room(room_code)
         _rooms[room_code] = room
 
         room.add_player(msg['name'], wsock)
 
-    wsock.send(json.dumps({'room': room_code}))
+    wsock.send(json.dumps({'msg': 'room-created', 'room': room_code}))
 
 
 def join_room(msg, wsock: WebSocket):
@@ -37,8 +37,29 @@ def join_room(msg, wsock: WebSocket):
 
         room.add_player(name, wsock)
 
+    __broadcast_player_joined_msg(msg['name'], room.players.items())
+    __send_join_success(wsock,
+                        [x for x in room.players if x != msg['name']])
 
-def _generate_room_code():
+
+def __broadcast_player_joined_msg(new_player_name, players):
+    msg = json.dumps({
+        'msg': 'player-joined',
+        'name': new_player_name
+    })
+    for player, player_wsock in players:
+        if player != new_player_name:
+            player_wsock.send(msg)
+
+
+def __send_join_success(wsock, player_list):
+    msg = json.dumps({
+        'msg': 'joined-room',
+        'players': player_list
+    })
+    wsock.send(msg)
+
+def __generate_room_code():
     return ''.join(random.choice(string.ascii_uppercase) for x in range(6))
 
 
